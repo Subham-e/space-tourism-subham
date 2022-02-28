3ce499e696009e41d63deef2e658b106cd7cdcfb
Subham Rai <93194290+Subham-e@users.noreply.github.com>
2022-02-28 23:08:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,66 +10,66 @@ toggleBtn.addEventListener('click', function(err) {
 });
 
 switch (window.location.pathname) {
-  case 'crew-commander.html':
+  case '/space-tourism-subham/crew-commander.html':
     document.querySelector(".commander").classList.add('activeCrewLink');
       document.querySelector(".crew").classList.add('activeNav');
     break;
 
-    case '/crew-engineer.html':
+    case '/space-tourism-subham/crew-engineer.html':
       document.querySelector(".engineer").classList.add('activeCrewLink');
           document.querySelector(".crew").classList.add('activeNav');
       break;
 
-      case '/crew-pilot.html':
+      case '/space-tourism-subham/crew-pilot.html':
         document.querySelector(".pilot").classList.add('activeCrewLink');
             document.querySelector(".crew").classList.add('activeNav');
         break;
 
-        case '/crew-specialist.html':
+        case '/space-tourism-subham/crew-specialist.html':
           document.querySelector(".specialist").classList.add('activeCrewLink');
               document.querySelector(".crew").classList.add('activeNav');
           break;
 
-          case '/destination-moon.html':
+          case '/space-tourism-subham/destination-moon.html':
             document.querySelector(".moon").classList.add('activeDestinationLink');
             document.querySelector(".destination").classList.add('activeNav');
             break;
 
-            case '/destination-europa.html':
+            case '/space-tourism-subham/destination-europa.html':
               document.querySelector(".europa").classList.add('activeDestinationLink');
               document.querySelector(".destination").classList.add('activeNav');
 
               break;
 
-              case '/destination-mars.html':
+              case '/space-tourism-subham/destination-mars.html':
                 document.querySelector(".mars").classList.add('activeDestinationLink');
                 document.querySelector(".destination").classList.add('activeNav');
 
                 break;
 
-                case '/destination-titan.html':
+                case '/space-tourism-subham/destination-titan.html':
                   document.querySelector(".titan").classList.add('activeDestinationLink');
                   document.querySelector(".destination").classList.add('activeNav');
 
                   break;
 
-                  case '/index.html':
+                  case '/space-tourism-subham/index.html':
                     document.querySelector(".home").classList.add("activeNav");
                     break;
 
-                        case '/technology-capsule.html':
+                        case '/space-tourism-subham/technology-capsule.html':
                           document.querySelector(".technology").classList.add("activeNav");
                           document.querySelector(".technologyImage").src="assets/technology/image-space-capsule-portrait.jpg";
                           document.querySelector(".capsule").classList.add("activeTechnologyLinks");
                           break;
 
-                             case '/technology-vehicle.html':
+                             case '/space-tourism-subham/technology-vehicle.html':
                              document.querySelector(".technology").classList.add("activeNav");
                              document.querySelector(".technologyImage").src="assets/technology/image-launch-vehicle-portrait.jpg";
                              document.querySelector(".vehicle").classList.add("activeTechnologyLinks");
                               break;
 
-                             case '/technology-spaceport.html':
+                             case '/space-tourism-subham/technology-spaceport.html':
                              document.querySelector(".technology").classList.add("activeNav");
 
                              document.querySelector(".technologyImage").src="assets/technology/image-spaceport-portrait.jpg";
